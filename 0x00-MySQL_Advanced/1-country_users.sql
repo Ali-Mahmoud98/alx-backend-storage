c03222f09c010b8c54f8442e7d499224a2784ba9
ali-mahmoud98 <ali98mahmoudr@gmail.com>
2024-08-10 20:37:36 +0300

    .

--- 0x00-MySQL_Advanced/1-country_users.sql
+++ 0x00-MySQL_Advanced/1-country_users.sql
@@ -1,3 +1,5 @@
+-- Create table users with id, email, name and country
+-- country is enum and defaults to US
 CREATE TABLE IF NOT EXISTS users (
     id INT NOT NULL AUTO_INCREMENT,
     email VARCHAR(255) NOT NULL UNIQUE,
