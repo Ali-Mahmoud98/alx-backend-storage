84905d04d8ca24d112f5ffba2329d00c19206c78
ali-mahmoud98 <ali98mahmoudr@gmail.com>
2024-08-10 20:27:54 +0300

    .

--- 0x00-MySQL_Advanced/0-uniq_users.sql
+++ 0x00-MySQL_Advanced/0-uniq_users.sql
@@ -1,7 +1,7 @@
 -- Creates table users with unique email
 CREATE TABLE IF NOT EXISTS users (
     id INT NOT NULL AUTO_INCREMENT,
-    name VARCHAR(255) NOT NULL,
+    name VARCHAR(255),
     email VARCHAR(255) NOT NULL UNIQUE,
     PRIMARY KEY (id)
 );
